bb9b414c92e751695b8124d0a3318ceeb4dbb0f8
ehddnr177 <ehddnr177@naver.com>
2023-04-05 14:31:22 +0900

    Initial commit

---
+++ src/main/java/org/pwdw/media_upload_test/MediaUploadTestApplication.java
@@ -0,0 +1,13 @@
+package org.pwdw.media_upload_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaUploadTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaUploadTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pwdw/media_upload_test/MediaUploadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pwdw.media_upload_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaUploadTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
